524f5d8abc75b1b88e60294f389a679271323a63
Matt Sievers <msievers@thegeneral.com>
2020-04-15 08:31:27 -0500

    Week 3 DQ's and assignment initial commit.

--- src/Week3/AssignmentWeek3.java
+++ src/Week3/AssignmentWeek3.java
@@ -22,13 +22,6 @@ public class AssignmentWeek3 {
     return charArray;
   }
 
-  public void printArray(char[] inputArray) {
-    for (int i = 0; i < inputArray.length; i++) {
-      System.out.println(inputArray[i]);
-    }
-
-  }
-
   public void checkEqualChars(char[] inputArray) {
     Stack<Character> stack = new Stack<>();
     for (int i = 0; i < inputArray.length; i++) {
--- src/Week3/Week3Driver.java
+++ src/Week3/Week3Driver.java
@@ -11,17 +11,31 @@ public class Week3Driver {
     AssignmentWeek3 assignmentWeek3 = new AssignmentWeek3();
     String inputFile1 = "/Users/ntqams30/Dropbox/GCU Work/CST-201/src/Week3/input1.txt";
     String inputFile2 = "/Users/ntqams30/Dropbox/GCU Work/CST-201/src/Week3/input2.txt";
+    String inputFile3 = "/Users/ntqams30/Dropbox/GCU Work/CST-201/src/Week3/input3.txt";
+    String inputFile4 = "/Users/ntqams30/Dropbox/GCU Work/CST-201/src/Week3/input4.txt";
 
+    //Extra (
     char[] file1 = assignmentWeek3.createCharArray(inputFile1);
+    //Will show balanced, but order is out of place ({)} (false positive)
     char[] file2 = assignmentWeek3.createCharArray(inputFile2);
+    //Working and balanced positive test.
+    char[] file3 = assignmentWeek3.createCharArray(inputFile3);
+    //Starts with closers and ends with openers, causes no stack exception
+    char[] file4 = assignmentWeek3.createCharArray(inputFile4);
 
     System.out.println("Input from file 1: ");
     System.out.println(file1);
     System.out.println("Input from file 2: ");
     System.out.println(file2);
+    System.out.println("Input from file 3: ");
+    System.out.println(file3);
+    System.out.println("Input from file 4: ");
+    System.out.println(file4);
 
     assignmentWeek3.checkEqualChars(file1);
     assignmentWeek3.checkEqualChars(file2);
+    assignmentWeek3.checkEqualChars(file3);
+    assignmentWeek3.checkEqualChars(file4);
 
   }
 }
